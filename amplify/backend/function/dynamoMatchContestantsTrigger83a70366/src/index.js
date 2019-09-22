3f25bd11753678d94c7383ab9ff921cc1937ca2b
james <jmosul@gmail.com>
2019-09-22 18:17:32 +0100

    refactor: readd function

---
+++ amplify/backend/function/dynamoMatchContestantsTrigger83a70366/src/index.js
@@ -0,0 +1,26 @@
+const CalculateCompetitorStats = require('./CalculateCompetitorStats');
+
+
+exports.handler = function(event, context) {
+  const promises = [];
+
+  event.Records.forEach((record) => {
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+
+    const handler = new CalculateCompetitorStats(
+      record.NewImage.leagueId.S,
+      record.NewImage.competitorId.S
+    );
+
+    promises.push(
+      handler.handle()
+    );
+  });
+
+
+  Promise.all(promises)
+    .then(() => context.done(null, 'Successfully processed DynamoDB record'))
+    .catch(() => context.done('Error processed DynamoDB record', null))
+};
